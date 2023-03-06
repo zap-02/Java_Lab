47cf4be4b52104a8a41539a691d6f0fa5f722783
zap-02 <71002930+zap-02@users.noreply.github.com>
2023-03-06 18:36:01 +0300

    Update and rename Lab1_extra_ex.java to Lab1ExtraEx.java

--- Lab1/Lab1ExtraEx.java
+++ Lab1/Lab1ExtraEx.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Lab1_extra_ex {
+public class Lab1ExtraEx {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
