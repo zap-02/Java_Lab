b126779eab6b197a1f030713bc9e1890d4d57759
Rachu <70590717+RachUwU@users.noreply.github.com>
2023-03-06 19:53:28 +0300

    Update Lab1Ex10Corrected.java

--- Lab1/Lab1Ex10Corrected.java
+++ Lab1/Lab1Ex10Corrected.java
@@ -1,10 +1,19 @@
+package ch1;
+
 import java.util.Random;
+import java.util.Scanner;
+
 public class Lab1Ex10Corrected {
     public static void main(String[] args) {
-        Random rand = new Random();
-        long value = rand.nextLong();
-        System.out.println("Value: " + value);
-        String result = Long.toUnsignedString(value, 36);
-        System.out.println("Result: " + result);
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < 30; i++) {
+            Random rand = new Random();
+            long value = rand.nextLong();
+            String str = Long.toUnsignedString(value, 36);
+            result.append(str);
+        }
+        System.out.println(result.substring(0, n));
     }
 }
