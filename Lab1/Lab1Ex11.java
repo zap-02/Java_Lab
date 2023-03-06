b4f234f67608f7bafe0ad687abc3166c0eab0d1c
zap-02 <71002930+zap-02@users.noreply.github.com>
2023-03-06 18:33:06 +0300

    Update and rename Lab1_Ex11.java to Lab1Ex11.java

--- Lab1/Lab1Ex11.java
+++ Lab1/Lab1Ex11.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.nio.charset.Charset;
 
-public class Lab1_Ex11 {
+public class Lab1Ex11 {
     public static void main(String[] args) {
         System.out.print("Enter string:");
         Scanner in = new Scanner(System.in);
