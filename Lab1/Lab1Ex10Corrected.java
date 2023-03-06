925a86b26522056aee62b8bbb7070b01646f1679
zap-02 <71002930+zap-02@users.noreply.github.com>
2023-03-06 18:36:34 +0300

    Update Lab1Ex10Corrected.java

--- Lab1/Lab1Ex10Corrected.java
+++ Lab1/Lab1Ex10Corrected.java
@@ -1,5 +1,5 @@
 import java.util.Random;
-public class Lab1Ex10 {
+public class Lab1Ex10Corrected {
     public static void main(String[] args) {
         Random rand = new Random();
         long value = rand.nextLong();
