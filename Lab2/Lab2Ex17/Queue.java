c3d4165a7d5fc2799f97228583eb6c5f6174d5ba
Igor Kondratev <71003957+igorkondratev@users.noreply.github.com>
2023-03-20 18:26:16 +0300

    Create Queue.java

---
+++ Lab2/Lab2Ex17/Queue.java
@@ -0,0 +1,72 @@
+package Lab2Ex16;
+
+import java.util.Iterator;
+
+public class Queue implements Iterable<String> {
+    private Node head; // голова очереди
+    private Node tail; // хвост очереди
+
+    private static class Node {
+        String data; // данные элемента
+        Node next; // ссылка на следующий узел
+
+        public Node(String data) {
+            this.data = data;
+        }
+    }
+
+    public void add(String data) {
+        Node node = new Node(data);
+        if (head == null) { // если очередь пуста, новый узел становится головой и хвостом
+            head = node;
+            tail = node;
+        } else { // иначе новый узел становится новым хвостом
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    public String remove() {
+        if (head == null) { // если очередь пуста, возвращаем null
+            return null;
+        }
+        String data = head.data;
+        head = head.next; // перемещаем голову на следующий узел
+        if (head == null) { // если после удаления головы очередь стала пустой, обнуляем и хвост
+            tail = null;
+        }
+        return data;
+    }
+  
+  @Override
+public Iterator<String> iterator() {
+    return new QueueIterator(head);
+}
+
+private class QueueIterator implements Iterator<String> {
+    private Node current;
+
+    public QueueIterator(Node head) {
+        this.current = head;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return current != null;
+    }
+
+    @Override
+    public String next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        String data = current.data;
+        current = current.next;
+        return data;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
