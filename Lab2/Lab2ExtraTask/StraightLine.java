8f9c8b2283fa1df9931a71b4b24b1e19b6c1bd5e
Maksim <maksimvrus371@gmail.com>
2023-03-27 20:37:02 +0300

    changes

--- Lab2/Lab2ExtraTask/StraightLine.java
+++ Lab2/Lab2ExtraTask/StraightLine.java
@@ -9,18 +9,9 @@ public class StraightLine {
         koefB = (b.getX() - a.getX());
         koefC = (a.getX()*(b.getY() - a.getY()) - a.getY()*(b.getX() - a.getX()));
     }
-    StraightLine(double a, double b, double c){
-        koefA = a;
-        koefB = b;
-        koefC = c;
-    }
 
     public double getKoefA() {
         return koefA;
-    }
-
-    public double getKoefB() {
-        return koefB;
     }
 
     public double getKoefC() {
--- Lab2/Lab2ExtraTask/Triangle.java
+++ Lab2/Lab2ExtraTask/Triangle.java
@@ -27,22 +27,15 @@ public class Triangle {
     }
 
     public Point getBisectorIntersection() {
-        double ab = Math.sqrt(Math.pow(b.getX() - a.getX(), 2) + Math.pow(b.getY() - a.getY(), 2));
-        double bc = Math.sqrt(Math.pow(c.getX() - b.getX(), 2) + Math.pow(c.getY() - b.getY(), 2));
-        double ca = Math.sqrt(Math.pow(a.getX() - c.getX(), 2) + Math.pow(a.getY() - c.getY(), 2));
-
         StraightLine bisC = getInnerBisectorLine(this.a, this.b, this.c);
         StraightLine bisA = getInnerBisectorLine(this.c, this.b, this.a);
         bisC.set();
         bisA.set();
-        bisC.print();
-        bisA.print();
         double x = (bisA.getKoefC() - bisC.getKoefC())/ (bisC.getKoefA() - bisA.getKoefA());
         double y = bisC.getKoefA()*x + bisC.getKoefC();
         return new Point(x, y);
     }
     public StraightLine getInnerBisectorLine(Point pointA, Point pointB, Point pointC){
-        double ab = Math.sqrt(Math.pow(b.getX() - a.getX(), 2) + Math.pow(b.getY() - a.getY(), 2));
         double bc = Math.sqrt(Math.pow(c.getX() - b.getX(), 2) + Math.pow(c.getY() - b.getY(), 2));
         double ca = Math.sqrt(Math.pow(a.getX() - c.getX(), 2) + Math.pow(a.getY() - c.getY(), 2));
         double n = ca/bc;
