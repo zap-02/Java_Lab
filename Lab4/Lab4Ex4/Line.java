8ca73d08fde23126fe178a025fff52fa812a1dd0
Maksim <maksimvrus371@gmail.com>
2023-04-30 22:02:36 +0300

    changes

--- Lab4/Lab4Ex4/Line.java
+++ Lab4/Lab4Ex4/Line.java
@@ -3,16 +3,16 @@ package Lab4Ex4;
 import Lab4Ex1.Point;
 
 public class Line extends Shape{
-    private final Point pointTo;
-    public Line(Point pointFrom, Point pointTo) {
+    private final Point to;
+    public Line(Point pointFrom, Point to) {
         super(pointFrom);
-        this.pointTo = pointTo;
+        this.to = to;
     }
 
     @Override
     public Point getCenter() {
-        double centerX = (point.getX() + pointTo.getX()) / 2;
-        double centerY = (point.getY() + pointTo.getY()) / 2;
+        double centerX = (point.getX() + to.getX()) / 2;
+        double centerY = (point.getY() + to.getY()) / 2;
         return new Point(centerX, centerY);
     }
 }
