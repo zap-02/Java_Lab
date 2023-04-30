d149143ca823b568ed20945feecb96d71319a63e
Maksim <maksimvrus371@gmail.com>
2023-04-30 22:08:44 +0300

    changes

---
+++ Lab4/Color.java
@@ -0,0 +1,32 @@
+public enum Color {
+    BLACK(0, 0, 0),
+    RED(255, 0, 0),
+    BLUE(0, 0, 255),
+    GREEN(0, 255, 0),
+    CYAN(0, 255, 255),
+    MAGENTA(255, 0, 255),
+    YELLOW(255, 255, 0),
+    WHITE(255, 255, 255);
+
+    private final int red;
+    private final int green;
+    private final int blue;
+
+    Color(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+}
